1cc5a001089895b97b554909c4d1dbe70e2fadfc
Leiluci <125084403+Leiluci@users.noreply.github.com>
2026-03-01 17:54:34 +0100

    Update HeroSection.tsx

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -2,8 +2,15 @@
 
 export default function HeroSection() {
   return (
-    <section style={{ background: 'red', height: '500px' }}>
-      <h1 style={{ color: 'white' }}>TEST HERO</h1>
+    <section
+      style={{
+        backgroundImage: "url('https://placehold.co/1200x600')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '500px',
+      }}
+    >
+      <h1 style={{ color: 'white' }}>GLAMORA HERO</h1>
     </section>
   )
 }
